46100b28adef952692076f3fdeeeeae9da56bf1e
abe4154 <kodai.abe4145@gmail.com>
2024-12-12 23:12:01 +0900

    config.js変更

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-1_IpjPlAqMP', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4e1edv69k53jj22hq9v21jbgcr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4e70ion37j4l6ec4ga6i7cg93p', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
